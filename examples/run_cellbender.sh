d3a5787ee837da363f91dd9c12d5f35a1f57aac7
Aljes <aleksandr.binkevich@gmail.com>
2025-07-23 14:14:39 +0100

    Update example script

--- examples/run_cellbender.sh
+++ examples/run_cellbender.sh
@@ -2,11 +2,13 @@
 
 set -euo pipefail
 
-sample_table=examples/sample_table.csv
+sample_table=examples/sample_table_exclude_features.csv
 
 [[ -e "$sample_table" ]] || (echo "File $sample_table not found" && false)
 
 nextflow run main.nf \
   --sample_table $sample_table \
-  --version 0.3 \
-  -resume
+  --on_irods \
+  --exclude_features "Peaks,Multiplexing Capture,CRISPR Guide Capture" \
+  --ignore_extensions "bam,bz2" \
+  --output_dir "my-cellbender-v3-results"
