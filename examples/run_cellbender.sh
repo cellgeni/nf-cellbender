592e4ddd14c5ac5ae14c22ed403e69ed046dac3a
Aljes <aleksandr.binkevich@gmail.com>
2025-07-23 14:11:33 +0100

    Add additional  example files

--- examples/run_cellbender.sh
+++ examples/run_cellbender.sh
@@ -2,16 +2,11 @@
 
 set -euo pipefail
 
-sample_table=examples/sample_table_irods.tsv
+sample_table=examples/sample_table.csv
 
 [[ -e "$sample_table" ]] || (echo "File $sample_table not found" && false)
 
 nextflow run main.nf \
   --sample_table $sample_table \
-  --mapper starsolo \
-  --solo_quant GeneFull \
-  --fpr 0.01 \
   --version 0.3 \
-  --exclude_features Peaks \
-  --on_irods \
   -resume
